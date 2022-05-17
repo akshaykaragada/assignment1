6f4b56c4a862d7c44543bdcae9ba97a72c34cd86
unknown <dev@dijisuji.com>
2022-05-17 11:30:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Loading from './Loading';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState([]);
+  const [ImgUrl, setImgUrl] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 4000)
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(res => res.json())
+      .then(data => setUser(data),
+        setImgUrl(`https://avatars.dicebear.com/api/avataaars/seed.svg?options[mood][]=happy`)
+      )
+      .catch(err => console.log(err))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loading ? <Loading /> : user.map(user => {
+        return (
+          <div key={user.id}>
+            <div className="card">
+              <div class="wrap" >
+                <div class="imagebox">
+                  <img className="featured-image" src={ImgUrl.replace('seed', user.username)} alt="avatar" />
+                </div>
+                <div class="textbox">
+                  <h3 className="name">{user.name}</h3>
+                  <p><b>Email:</b> {user.email}</p>
+                  <p><b>Phone:</b> {user.phone}</p>
+                  <p><b>Company:</b> {user.company.name}</p>
+                  <p><b>Website:</b> {user.website}</p>
+                  <p><b>Address:</b> {user.address.street}, {user.address.suite}, {user.address.city}, {user.address.zipcode}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        )
+      })
+      }
     </div>
   );
 }
